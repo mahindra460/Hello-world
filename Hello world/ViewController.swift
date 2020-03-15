ef344716d2cd87a7538a240ff57c507088a442b2
Mahendra Reddy <mahendrareddy@Mahendras-MacBook-Air.local>
2020-03-15 12:24:54 +0530

    Hello World

---
+++ Hello world/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Hello world
+//
+//  Created by Mahendra Reddy on 15/03/20.
+//  Copyright © 2020 Mahendra Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Hello")
+    }
+
+
+}
+
